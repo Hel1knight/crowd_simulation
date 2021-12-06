b488556455b13ab3b8cbe053f9f171bc7f2787b9
Hel1knight <francois2712@hotmail.fr>
2021-12-06 18:46:55 +0100

    Update agent.cs
    Agent modifié

--- Assets/Scripts/agent.cs
+++ Assets/Scripts/agent.cs
@@ -4,54 +4,62 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
-    //public Transform goal;
-    GameObject[] targets;
-    private int i = 0;
-    public UnityEngine.AI.NavMeshAgent agent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        //agent.destination = goal.position;
-        //agent.SetDestination(GameObject.FindGameObjectWithTag("Arrivé").transform.position);
-        targets = GameObject.FindGameObjectsWithTag("Arrivé");
-        agent.destination = targets[i].transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        var dist = Vector3.Distance(targets[i].transform.position, transform.position);
-        //currentTarget = targets[i].transform;
-        //if npc reaches its destination (or gets close)...
-        if (dist < 2)
-        {
-            i++; //change next target      
-            if (i < targets.Length)
-            {
-                agent.destination = targets[i].transform.position; //go to next target by setting it as the new destination
-            }
-            
-            //check if at end of cycle, then reset to beginning of cycle
-            if (i == targets.Length)
-            {
-                Debug.Log("NAVIGATION FINISHED. RESET.");
-                i = 0;
-                agent.destination = targets[i].transform.position;
-            }
-        }
-    }
-    //public Transform goal;
+    ////public Transform goal;
+    //GameObject[] targets;
+    //private int i = 0;
+    //public UnityEngine.AI.NavMeshAgent agent;
     //// Start is called before the first frame update
     //void Start()
     //{
-    //    UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-    //    agent.destination = goal.position;
+    //    agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    //    //agent.destination = goal.position;
+    //    //agent.SetDestination(GameObject.FindGameObjectWithTag("Arrivé").transform.position);
+    //    targets = GameObject.FindGameObjectsWithTag("Arrivé");
+    //    agent.destination = targets[i].transform.position;
     //}
 
     //// Update is called once per frame
     //void Update()
     //{
+    //    var dist = Vector3.Distance(targets[i].transform.position, transform.position);
 
+    //    Debug.Log(dist);
+
+
+    //    //currentTarget = targets[i].transform;
+    //    //if npc reaches its destination (or gets close)...
+    //    //if (dist < 2)
+    //    //{
+    //    //    for (i = 0; i <= targets.Length; i++)
+    //    //    {
+
+
+    //    //        //i++; //change next target      
+    //    //        if (i < targets.Length)
+    //    //        {
+    //    //            agent.destination = targets[i].transform.position; //go to next target by setting it as the new destination
+    //    //        }
+    //    //    }
+    //    //    //check if at end of cycle, then reset to beginning of cycle
+    //    //    //if (i == targets.Length)
+    //    //    //{
+    //    //    //    Debug.Log("NAVIGATION FINISHED. RESET.");
+    //    //    //    i = 0;
+    //    //    //    agent.destination = targets[i].transform.position;
+    //    //    //}
+    //    //}
     //}
+    public Transform goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
